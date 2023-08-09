177e248ce379edfd7a7568b3b5b498b59842edb1
leonardoovaldezz <leonardocamvaldez12@hotmail.com>
2023-08-08 18:03:09 -0600

    Agregando archivos y configuraciones iniciales

---
+++ src/components/00HelloWorld.vue
@@ -0,0 +1,12 @@
+<template>
+  <h2>Hola Mundo VUE</h2>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <hello-world />
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/00HelloWorld";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
