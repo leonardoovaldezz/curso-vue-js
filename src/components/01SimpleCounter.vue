41a053df2b09e494f05b50d96f62eed0f6e9710b
leonardoovaldezz <leonardocamvaldez12@hotmail.com>
2023-08-08 18:35:25 -0600

    01SimpleCounter

---
+++ src/components/01SimpleCounter.vue
@@ -0,0 +1,32 @@
+<template>
+  <h2>Contador Simple</h2>
+  <p>Contador: {{ counter }}</p>
+  <button @click="increment">Incrementar</button>
+  <button @click="decrement">Decrementar</button>
+</template>
+  
+  <script>
+import { ref } from "vue";
+export default {
+  name: "SimpleCounter",
+  setup() {
+    const counter = ref(0); 
+    const increment = () => {
+      counter.value++;
+    };
+    const decrement = () => {
+      counter.value--;
+    };
+
+    return {
+      counter,
+      increment,
+      decrement,
+    };
+  },
+};
+</script>
+  
+  <style>
+</style>
+  
