c5c14b2da33fee28123e6737285b3539bd8985e3
leonardoovaldezz <leonardocamvaldez12@hotmail.com>
2023-08-08 18:35:12 -0600

    01SimpleCounter

--- src/components/00HelloWorld.vue
+++ src/components/00HelloWorld.vue
@@ -1,10 +1,19 @@
 <template>
   <h2>Hola Mundo VUE</h2>
+  <p>{{ message }}</p>
+  <label for="message">Mensaje</label><br />
+  <input type="text" id="message" v-model="message" />
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
+  setup() {
+    const message = "Mensaje Hello World";
+    return {
+      message,
+    };
+  },
 };
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
+  <simple-counter />
+  <hr />
   <hello-world />
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/00HelloWorld";
-
+import SimpleCounter from "@/components/01SimpleCounter";
 export default {
   name: "Home",
   components: {
     HelloWorld,
+    SimpleCounter,
   },
 };
 </script>
